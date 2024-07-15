789c914eb82856470ba72c3e9ba05ec7d9a971cb
Matheo-bis <matheomarcout@tutamail.com>
2024-07-15 15:57:36 +0200

    Adding movement to buttons

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -9,6 +9,7 @@ const CHAR_LENGTH := 0.025
 		_label_object.text = label
 		print("New label: "+label)
 		update_size(newLabel)
+@export var movement := 0.01
 
 @onready var _hitbox : CollisionShape3D = $CollisionShape3D
 @onready var _mesh : MeshInstance3D = $MeshInstance3D
@@ -17,3 +18,15 @@ const CHAR_LENGTH := 0.025
 func update_size(newLabel : String):
 	_hitbox.shape.size.x = newLabel.length() * CHAR_LENGTH
 	_mesh.mesh.size.x = newLabel.length() * CHAR_LENGTH
+
+func _ready():
+	_label_object.text = label
+
+func _on_body_entered(_body):
+	position.z -= movement
+	pass # Replace with function body.
+
+
+func _on_body_exited(body):
+	position.z += movement
+	pass # Replace with function body.
