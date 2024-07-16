77ded69e90f3cedb9845d6031b916169e591abf3
Matheo-bis <matheomarcout@tutamail.com>
2024-07-16 16:42:00 +0200

    Documentation for button.gd

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -1,31 +1,41 @@
 @tool
 extends Area3D
+## Tool script for a self-resizing button.
 
+
+## Similar to [signal Area3D.body_entered] but passes the node name instead of
+## the coliding object.
 signal node_entered(emitter : String)
 
-const CHAR_LENGTH := 0.025
+const CHAR_LENGTH := 0.025 ## The length of a character, in meters.
 
+## The label of the button
 @export var label := "Label" :
 	set(newLabel):
 		label = newLabel
 		if _label_object != null : _label_object.text = label
 		print("New label: "+label)
-		if _label_object != null : update_size(newLabel)
+		if _label_object != null : _update_size(newLabel)
+
+## The movement of the button when pressed, in meters
 @export var movement := 0.01
 
 @onready var _hitbox : CollisionShape3D = $CollisionShape3D
 @onready var _mesh : MeshInstance3D = $MeshInstance3D
 @onready var _label_object : Label3D = $Label3D
 
-func update_size(newLabel : String):
+
+func _update_size(newLabel : String):
 	_hitbox.shape.size.x = newLabel.length() * CHAR_LENGTH
 	_mesh.mesh.size.x = newLabel.length() * CHAR_LENGTH
+
 
 func _ready():
 	_label_object.text = label
 	_hitbox.shape = _hitbox.shape.duplicate()
 	_mesh.mesh = _mesh.mesh.duplicate()
-	update_size(label)
+	_update_size(label)
+
 
 func _on_body_entered(_body):
 	_mesh.position.z -= movement
