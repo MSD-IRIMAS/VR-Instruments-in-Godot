d19bd7239bb3336ff1a66d4928a501ab7001dabe
Matheo-bis <matheomarcout@tutamail.com>
2024-07-16 16:17:58 +0200

    Document and format xylophone.gd

--- Scripts/xylophone.gd
+++ Scripts/xylophone.gd
@@ -1,135 +1,150 @@
 extends Node
+## The first version of the Xylophone script
+##
+## The first version of the Xylophone script, supports polyphonia on one single
+## speaker and multiple instrument sounds via the implementation of the Fourier
+## Serie System.
 
 
+## The modes avaiables.
 enum MODES {SINGLE_NOTE, POLYPHONIC, FOURIER_SINGLE}
 
+## The speed at wich the note will play at full volume, in meters per second.
 @export var MAX_SPEED := 4.0
-@export var movement := 0.01
-@export var mode := MODES.SINGLE_NOTE
+@export var movement := 0.01 ## The movement of the keys, in meters
+@export var mode := MODES.SINGLE_NOTE ## The current mode.
+
+## The current instrument (usefull only in FOURIER_SINGLE mode).
 @export var instrument := Music.INSTRUMENTS_NAMES.SINE :
 	set(new_instrument):
 		print("New instrument: "+Music.INSTRUMENTS_NAMES.find_key(new_instrument))
 		instrument = new_instrument
-		instrument_serie = FourierSerie.new( \
-		Music.INSTRUMENTS[Music.INSTRUMENTS_NAMES.find_key(instrument)], \
-		440)
+		_instrument_serie = FourierSerie.new( \
+				Music.INSTRUMENTS[Music.INSTRUMENTS_NAMES.find_key(instrument)], \
+				440)
 		$Label3D.text = Music.INSTRUMENTS_NAMES.find_key(instrument)
 
-@onready var Notes := $Notes
-@onready var Player := $Player
+@onready var _Notes := $Notes
+@onready var _Player := $Player 
 
-@onready var instrument_serie := FourierSerie.new( \
+@onready var _instrument_serie := FourierSerie.new( \
 		Music.INSTRUMENTS[Music.INSTRUMENTS_NAMES.find_key(instrument)], \
 		440)
 
-var sample_hz := 22050.0 # Keep the number of samples to mix low, GDScript is not super fast.
-var pulse_hz := 440.0
-var phase := 0.0
-var active_notes : Array[String] = []
+var _sample_hz := 22050.0 # Keep the number of samples to mix low, GDScript is not super fast.
+var _pulse_hz := 440.0
+var _phase := 0.0
+var _active_notes : Array[String] = []
 
-var playback : AudioStreamPlayback = null
-var playing := false
+var _playback : AudioStreamPlayback = null
+var _playing := false
+
 
 func _reset_buffer() -> void:
 	# Setting mix rate is only possible before play().
-	Player.stream.mix_rate = sample_hz
-	Player.play()
-	playback = Player.get_stream_playback()
+	_Player.stream.mix_rate = _sample_hz
+	_Player.play()
+	_playback = _Player.get_stream_playback()
 	# WARNING `_fill_buffer` must be called *after* setting `playback`,
 	# as `fill_buffer` uses the `playback` member variable.
 	#_fill_buffer()
+
 
 func _ready() -> void:
 	_reset_buffer()
 
 
-func fill_buffer() -> void:
-	var increment := 1 / sample_hz
+func _fill_buffer() -> void:
+	var increment := 1 / _sample_hz
 	
-	var to_fill : int = playback.get_frames_available()
+	var to_fill : int = _playback.get_frames_available()
 	# TESTING 
 	#print(to_fill)
 	while to_fill > 0:
-		playback.push_frame(Vector2.ONE * sin(phase * pulse_hz * TAU)) # Audio frames are stereo.
-		phase = fmod(phase + increment, 1.0)
+		_playback.push_frame(Vector2.ONE * sin(_phase * _pulse_hz * TAU)) # Audio frames are stereo.
+		_phase = fmod(_phase + increment, 1.0)
 		to_fill -= 1
 
 
-func fill_buffer_polyphonic() -> void:
-	var increment := 1 / sample_hz
+func _fill_buffer_polyphonic() -> void:
+	var increment := 1 / _sample_hz
 	
-	var to_fill : int = playback.get_frames_available()
+	var to_fill : int = _playback.get_frames_available()
 	# TESTING 
 	#print(to_fill)
 	while to_fill > 0:
 		var x: float = 0.0
-		for note in active_notes:
+		for note in _active_notes:
 			var pulse : float = Music.A * (1 + Music.NOTES[note]/12) \
 					if Music.NOTES[note] >= 0 \
 					else Music.A * 1/(2**(-Music.NOTES[note]/12))
-			x += sin(phase * pulse * TAU)
-		playback.push_frame(Vector2.ONE * x) # Audio frames are stereo.
-		phase = fmod(phase + increment, 1.0)
+			x += sin(_phase * pulse * TAU)
+		_playback.push_frame(Vector2.ONE * x) # Audio frames are stereo.
+		_phase = fmod(_phase + increment, 1.0)
 		to_fill -= 1
 
-func fill_buffer_serie(serie : FourierSerie) -> void:
-	var increment := 1 / sample_hz
+
+func _fill_buffer_serie(serie : FourierSerie) -> void:
+	var increment := 1 / _sample_hz
 	
-	var to_fill : int = playback.get_frames_available()
+	var to_fill : int = _playback.get_frames_available()
 	# TESTING 
 	#print(to_fill)
 	while to_fill > 0:
-		var x := serie.compute(phase)
+		var x := serie.compute(_phase)
 		#print(x)
-		playback.push_frame(Vector2.ONE * x) # Audio frames are stereo.
-		phase = fmod(phase + increment, 1.0)
+		_playback.push_frame(Vector2.ONE * x) # Audio frames are stereo.
+		_phase = fmod(_phase + increment, 1.0)
 		to_fill -= 1
+
 
 ## DEPRECATED: Use fill_buffer instead
 ## @deprecated
-func push() -> void:
-	var increment := pulse_hz / sample_hz
-	playback.push_frame(Vector2.ONE * sin(phase * TAU)) # Audio frames are stereo.
-	phase = fmod(phase + increment, 1.0)
+func _push() -> void:
+	var increment := _pulse_hz / _sample_hz
+	_playback.push_frame(Vector2.ONE * sin(_phase * TAU)) # Audio frames are stereo.
+	_phase = fmod(_phase + increment, 1.0)
+
 
 func _process(_delta : float) -> void:
 	# TESTING
 	#print(phase)
-	if playing : 
-		if mode == MODES.SINGLE_NOTE: fill_buffer()
-		if mode == MODES.POLYPHONIC: fill_buffer_polyphonic()
-		if mode == MODES.FOURIER_SINGLE: fill_buffer_serie(instrument_serie)
+	if _playing : 
+		if mode == MODES.SINGLE_NOTE: _fill_buffer()
+		if mode == MODES.POLYPHONIC: _fill_buffer_polyphonic()
+		if mode == MODES.FOURIER_SINGLE: _fill_buffer_serie(_instrument_serie)
 
-func _on_notes_note_begin(note: String, _velocity: float) -> void:
+
+func _on_notes_note_begin(note: String, velocity: float) -> void:
 	# TESTING 
 	#print(note + " note entered")
 	#print(remap(_velocity, 0, MAX_SPEED, 0, 1))
-	Player.volume_db = linear_to_db(remap(_velocity, 0, MAX_SPEED, 0, 1))
+	_Player.volume_db = linear_to_db(remap(velocity, 0, MAX_SPEED, 0, 1))
 	
-	var mesh := Notes.find_child(note).get_node(^"MeshInstance3D") as MeshInstance3D
+	var mesh := _Notes.find_child(note).get_node(^"MeshInstance3D") as MeshInstance3D
 	mesh.position.y -= movement
 	
 	# TESTING 
 	#print(NOTES[note])
-	active_notes.append(note)
-	pulse_hz = Music.A * (1 + Music.NOTES[note]/12) if Music.NOTES[note] >= 0 \
+	_active_notes.append(note)
+	_pulse_hz = Music.A * (1 + Music.NOTES[note]/12) if Music.NOTES[note] >= 0 \
 			else Music.A * 1/(2**(-Music.NOTES[note]/12))
-	instrument_serie.base_pulse = pulse_hz
+	_instrument_serie.base_pulse = _pulse_hz
 	# TESTING 
 	#print(instrument_serie.base_pulse)
 	
 	_reset_buffer()
-	playing = true
+	_playing = true
 
 
 func _on_notes_note_end(note: String) -> void:
 	# TESTING 
 	#print(note + " note exited")
 	
-	var mesh := Notes.find_child(note).get_node("MeshInstance3D") as MeshInstance3D
+	var mesh := _Notes.find_child(note).get_node("MeshInstance3D") as MeshInstance3D
 	mesh.position.y += movement
 	
-	active_notes.remove_at(active_notes.find(note))
+	_active_notes.remove_at(_active_notes.find(note))
 	
-	playing = false
+	_playing = false
 	#Player.stop()
