d645b638d4381da7ca43de4592c97b1abb05e9d8
Matheo-bis <102729132+Matheo-bis@users.noreply.github.com>
2024-07-24 11:51:03 +0200

    Fix hand detection list (movement is still mirorred)

--- Scripts/bpm_detector.gd
+++ Scripts/bpm_detector.gd
@@ -18,14 +18,16 @@ var last_beats : int = 4 ## The last retrived Beats value.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta : float) -> void:
+	var error := false
 	main_node = null
 	
 	# Go trough the nodes to find one active
-	for node : Node3D in followed_nodes:
+	for node : Hand in followed_nodes:
 		if node.active and main_node == null : main_node = node
 		elif node.active and main_node != null : 
 			_label.text = "Two nodes can't be active \n at the same time!"
 			main_node = null
+			error = true
 			break
 	
 	if main_node != null:
@@ -48,13 +50,15 @@ func _process(_delta : float) -> void:
 		_metronome.bpm = last_value
 		_metronome.beats = last_beats
 		
-		if not followed_nodes.is_empty() :
+		if not followed_nodes.is_empty() and not error:
 			_label.text = "Please pess the grip button 
 					of one controller.
 					Last recorded BPM: " + str(snappedf(last_value, 0.1))
 
 
 func _on_detection_zone_body_entered(body : Node3D) -> void:
+	#TESTING
+	#print("node "+body.name+" entered")
 	if body is Hand :
 		followed_nodes.append(body)
 	elif body.get_parent() is Hand :
@@ -62,7 +66,10 @@ func _on_detection_zone_body_entered(body : Node3D) -> void:
 
 
 func _on_detection_zone_body_exited(body : Node3D) -> void:
+	#TESTING
+	#print("node "+body.name+" exited")
 	followed_nodes.erase(body)
+	followed_nodes.erase(body.get_parent())
 	if followed_nodes.is_empty() : 
 		_label.text = "Please stand on the
 				platfrom to begin.
